c07c04fdc85f1be5212880066e298b9b39dc9995
yiyuezhuo <yiyuezhuo@gmail.com>
2021-12-04 12:17:52 +0800

    Remap Shader Script

---
+++ Assets/Mapshower.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapshower : MonoBehaviour
+{
+    int width;
+    int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var material = GetComponent<Renderer>().material;
+        var mainTex = material.GetTexture("_MainTex") as Texture2D;
+        var mainArr = mainTex.GetPixels32();
+
+        width = mainTex.width;
+        height = mainTex.height;
+
+        var main2remap = new Dictionary<Color32, Color32>();
+        var remapArr = new Color32[mainArr.Length];
+        int idx = 0;
+        for(int i=0; i<mainArr.Length; i++){
+            var mainColor = mainArr[i];
+            if(!main2remap.ContainsKey(mainColor)){
+                var low = (byte)(idx % 256);
+                var high = (byte)(idx / 256);
+                main2remap[mainColor] = new Color32(low, high, 0, 255);
+                idx++;
+            }
+            var remapColor = main2remap[mainColor];
+            remapArr[i] = remapColor;
+        }
+
+        var paletteArr = new Color32[256*256];
+        for(int i=0; i<paletteArr.Length; i++){
+            paletteArr[i] = new Color32(255, 255, 255, 255);
+        }
+
+        var remapTex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        remapTex.filterMode = FilterMode.Point;
+        remapTex.SetPixels32(remapArr);
+        remapTex.Apply(false);
+        material.SetTexture("_RemapTex", remapTex);
+
+        var paletteTex = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        paletteTex.filterMode = FilterMode.Point;
+        paletteTex.SetPixels32(paletteArr);
+        paletteTex.Apply(false);
+        material.SetTexture("_PaletteTex", paletteTex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
